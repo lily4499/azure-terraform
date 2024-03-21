4d30aa6f6319930332d5b2df7f416ec7cfe17d46
lily4499 <77320224+lily4499@users.noreply.github.com>
2024-03-21 12:05:53 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                     = var.acr_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.azure_region
+  sku                      = "Basic"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  location            = var.azure_region
+  resource_group_name = var.resource_group_name
+  dns_prefix          = var.aks_name
+  kubernetes_version  = "1.20.9"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_D2_v2"
+  }
+}
