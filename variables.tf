27b6a07fcfa47b13078086cff9fafd6cfbb7ef1e
lily4499 <77320224+lily4499@users.noreply.github.com>
2024-03-22 09:00:39 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,18 @@
-variable "ARM_SUBSCRIPTION_ID" {
-  description = "Azure Subscription ID"
-}
+//variable "ARM_SUBSCRIPTION_ID" {
+//  description = "Azure Subscription ID"
+//}
 
-variable "ARM_CLIENT_ID" {
-  description = "Azure Client ID"
-}
+//variable "ARM_CLIENT_ID" {
+ // description = "Azure Client ID"
+//}
 
-variable "ARM_CLIENT_SECRET" {
-  description = "Azure Client Secret"
-}
+//variable "ARM_CLIENT_SECRET" {
+//  description = "Azure Client Secret"
+//}
 
-variable "ARM_TENANT_ID" {
-  description = "Azure Tenant ID"
-}
+//variable "ARM_TENANT_ID" {
+//  description = "Azure Tenant ID"
+//}
 
 
 variable "acr_name" {
