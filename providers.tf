a7dee7fb24df4d46dffcfafea4613f0edd909239
lily4499 <77320224+lily4499@users.noreply.github.com>
2024-03-22 08:51:07 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,10 +1,9 @@
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  subscription_id = var.ARM_SUBSCRIPTION_ID
-  client_id       = var.ARM_CLIENT_ID
-  client_secret   = var.ARM_CLIENT_SECRET
-  tenant_id       = var.ARM_TENANT_ID
+  client_id     = var.TF_VAR_client_id
+  client_secret = var.TF_VAR_client_secret
+  tenant_id     = var.TF_VAR_tenant_id
 }
 
 # Azure Provider source and version being used
